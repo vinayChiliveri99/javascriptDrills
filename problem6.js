f7b9883443dbb22b4a37d91da2c39c627a5f1a44
vinaychiliverii <vinaychiliveri99@gmail.com>
2023-11-10 18:16:05 +0530

    problem6.js and testProblem6.js added

---
+++ problem6.js
@@ -0,0 +1,21 @@
+// ==== Problem #6 ====
+// A buyer is interested in seeing only BMW and Audi cars within the inventory.  Execute a function and return an array that only contains BMW and Audi cars.  Once you have the BMWAndAudi array, use JSON.stringify() to show the results of the array in the console.
+
+let inventory = require('./cars');
+
+function onlyBmwAudi(inventory) {
+  var bmwAudi = [];
+
+  for (let i = 0; i < inventory.length; i++) {
+    if (
+      inventory[i].car_make === 'BMW' ||
+      inventory[i].car_make === 'Audi'
+    ) {
+      bmwAudi.push(inventory[i]);
+    }
+  }
+
+  return bmwAudi;
+}
+
+module.exports = onlyBmwAudi;
